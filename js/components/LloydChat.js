f933f75f8e1ef7805c5649d7c15eb14d36aa4cbe
Kevin Baktiar <afterafx@gmail.com>
2019-03-08 09:54:54 -0800

    added correct links for Find YaMovie page

--- js/components/LloydChat.js
+++ js/components/LloydChat.js
@@ -10,7 +10,7 @@ export default class LloydChat extends HTMLElement {
   render() {
     this.innerHTML = `
     <div id="lloyd-outline">
-      <img src="./images/Lloyd.png" alt="" id="chatbot-btn" />
+      <img src="./images/Lloyd.png" alt="" href="./question-form.html" id="chatbot-btn" />
     </div>
     `;
   }
