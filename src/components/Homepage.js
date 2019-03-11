0e9b55ea34149e9ca027b8063508f39a0c738a63
Ivy <yunjilim@gmail.com>
2019-03-11 10:25:12 -0700

    started routes

---
+++ src/components/Homepage.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import LloydChat from './LloydChat';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>APp</h1>
+
+{/* // <section id="link-container">
+//   <div id="landing-quote">
+//     <h3>We find your movie,</h3>
+//     <h3>You bring the popcorn!</h3>
+//   </div>
+
+//   <div id="link-info">
+//     <a class="landing-link landing-color find" href="./question-form.html">Find YaMovie!</a>
+//     <!-- <a class="landing-link landing-color signup" href="">Sign Up!</a> -->
+//   </div>
+// </section>
+
+// <article id="overview">
+//   <img src="./images/logo-v3.png" alt="" />
+//   <span id="movie-summary">
+//     <p>
+//       Our technology intelligently takes your unique personality, viewing habits, and
+//       diverse interests to curate the right movies for you!
+//     </p>
+//   </span>
+// </article>
+
+// <section id="card-container">
+//   <div id="full-screen">
+//     <div class="row">
+//       <article class="col-xs-4">
+//         <div class="cards">
+//           <span class="glyphicon glyphicon-flash icon"></span>
+//           <hr class="divider" />
+//           <h2 class="title">Browse</h2>
+//           <div class="info">
+//             <hr class="divider" />
+//             <br>
+//             <p class="lead">
+//               Go through our collection of thousands of movies with up-to-date
+//               information, including critic ratings and links to streaming sources.
+//             </p>
+//           </div>
+//         </div>
+//       </article>
+//       <article class="col-xs-4">
+//         <div class="cards">
+//           <span class="glyphicon glyphicon-bookmark icon"></span>
+//           <hr class="divider" />
+//           <h2 class="title">Chatbot</h2>
+//           <div class="info">
+//             <hr class="divider" />
+//             <br>
+//             <p class="lead">
+//               On our site, you will meet Lloyd our chatbot! He will ask you a set of
+//               personalized questions to help you pinpoint the movie you are craving.
+//             </p>
+//           </div>
+//         </div>
+//       </article>
+//       <article class="col-xs-4">
+//         <div class="cards">
+//           <span class="glyphicon glyphicon-ice-lolly-tasted icon"></span>
+//           <hr class="divider" />
+//           <h2 class="title">Benefits</h2>
+//           <div class="info">
+//             <hr class="divider" />
+//             <br>
+//             <p class="lead">
+//               By signing up with YaMovie, we are able to take what you enjoy and
+//               curate recommendations
+//             </p>
+//           </div>
+//         </div>
+//       </article>
+//     </div>
+//   </div>
+// </section> */}
+
+      </div>
+    )
+  }
+}
+
+export default Homepage;
