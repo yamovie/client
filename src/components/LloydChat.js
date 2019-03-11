0848602ff57863c25deb049e3df84404112848df
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-03-11 15:04:14 -0700

    converted navbar and lloyd to stateless functions, switched lloyd back to Link instead of NavLink

--- src/components/LloydChat.js
+++ src/components/LloydChat.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../css/navbar.css';
 
-export default class LloydChat extends Component {
-  /**
-   * Renders the Lloyd chatbot button on the bottom right corner of the page.
-   */
-  render() {
-    return (
-      <div id="lloyd-outline">
-        <NavLink to="/questionform">
-          <img src="./images/Lloyd.png" alt="Talk to Lloyd!" id="chatbot-btn" />
-        </NavLink>
-      </div>
-    );
-  }
-}
+const LloydChat = () => (
+  <div id="lloyd-outline">
+    <Link to="/questionform">
+      <img src="./images/Lloyd.png" alt="Talk to Lloyd!" id="chatbot-btn" />
+    </Link>
+  </div>
+);
+
+export default LloydChat;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../css/navbar.css';
 
-export default class Navbar extends Component {
-  /**
-   * Renders the navbar on the page. Uses flexboxes to display information and links.
-   */
-  render() {
-    return (
-      <div>
-        <NavLink to="/">
-          <img
-            src="./images/logo-v3-white-whitepopcorn.png"
-            alt="YaMovie"
-            id="main-logo"
-          />
-        </NavLink>
-        <ul id="nav-bar-links">
-          <li>
-            <NavLink to="/browsepage">Browse</NavLink>
-          </li>
-          <li>
-            <NavLink to="/questionform">Find YaMovie</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+/**
+ * JSX used to render the navbar on the page. Uses flexboxes to display information and links.
+ * @returns JSX to create HTML navbar
+ */
+const Navbar = () => (
+  <div>
+    <NavLink to="/">
+      <img src="./images/logo-v3-white-whitepopcorn.png" alt="YaMovie" id="main-logo" />
+    </NavLink>
+    <ul id="nav-bar-links">
+      <li>
+        <NavLink to="/browsepage">Browse</NavLink>
+      </li>
+      <li>
+        <NavLink to="/questionform">Find YaMovie</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Navbar;
