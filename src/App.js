a2065a04feb4a5a3f6ff72e94cff8661fe4c4fa6
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-03-12 10:37:26 -0700

    fixed eslint errors in App and Homepage, made both stateless functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import BrowsePage from './components/BrowsePage';
 import AboutPage from './components/AboutPage';
 import MovieForm from './components/MovieForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Route exact path="/" component={ Homepage } >Home</Route>
-        <Switch>
-          <Route path="/browsepage" component={ BrowsePage } />
-          <Route path="/about" component={ AboutPage } />
-          <Route path="/movieform" component={ MovieForm } />
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Route exact path="/" component={Homepage}>
+      Home
+    </Route>
+    <Switch>
+      <Route path="/browsepage" component={BrowsePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/movieform" component={MovieForm} />
+    </Switch>
+  </div>
+);
 
 export default App;
