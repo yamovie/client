afa957f444808b7ec626c16dcda740c5467ac249
Ivy <yunjilim@gmail.com>
2019-03-12 09:42:35 -0700

    added movieform route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 // import BrowsePage from './components/BrowsePage';
 // import AboutPage from './components/AboutPage';
+import MovieForm from './components/MovieForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Route exact path="/" component={ Homepage } >Home</Route>
-        {/* <Switch>
-          <Route path="/browsepage" component={BrowsePage} />
-          <Route path="/about" component={AboutPage} />
-        </Switch> */}
+        <Route exact path="/" component={ Homepage } >Home</Route>
+        <Switch>
+          <Route path="/browsepage" component={ BrowsePage } />
+          <Route path="/about" component={ AboutPage } />
+          <Route path="/movieform" component={ MovieForm } />
+        </Switch>
       </div>
     );
   }
