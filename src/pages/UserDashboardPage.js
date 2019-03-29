816001e733ce87959604c471e8e08706212481e0
Hans Martin <hansmhank@gmail.com>
2019-03-29 14:01:13 -0700

    Add sidebar

--- src/pages/UserDashboardPage.js
+++ src/pages/UserDashboardPage.js
@@ -11,10 +11,51 @@ const userDashboardPage = () => (
       <aside className="account-nav">
         <ul>
           <li>
-            <a href="https://youtube.com">Account</a>
+            <a href="https://youtube.com">Account settings</a>
+          </li>
+          <li>
+            <a href="https://youtube.com">Movie preferences</a>
+          </li>
+          <li>
+            <a href="https://youtube.com">Watchlist</a>
+          </li>
+          <li>
+            <a href="https://youtube.com">Log out</a>
           </li>
         </ul>
       </aside>
+
+      <div className="account-page">
+        <form>
+          <div>
+            <h3>Streaming Subscriptions</h3>
+            <div>
+              <label>
+                Hulu
+                <input type="checkbox" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Netflix
+                <input type="checkbox" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Disney +
+                <input type="checkbox" />
+              </label>
+              <label>
+                ESPN
+                <input type="checkbox" />
+              </label>
+            </div>
+            
+           
+          </div>
+        </form>
+      </div>
     </div>
   </div>
   
