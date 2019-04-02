6703754a3578fc00bf4daa2b4899e190ef30636e
Ivy <yunjilim@gmail.com>
2019-04-02 16:38:03 -0700

    oauth working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-// import openSocket from 'socket.io-client';
+import queryString from 'query-string';
 import HomePage from './pages/HomePage';
 import BrowsePage from './pages/BrowsePage';
 import AboutPage from './pages/AboutPage';
@@ -8,26 +8,40 @@ import MovieForm from './components/MovieForm';
 import ChatWindow from './components/ChatWindow';
 import OAuth from './components/OAuth';
 import './css/main.css';
+import axios from 'axios';
 
-
-const provider = 'google';
-// const API_URL = 'https://yamovie-server.herokuapp.com/auth';
-// const socket = io('http://localhost');
-// const socket = openSocket('http://localhost:80');
-
-const App = () => (
-  <div className="App">
-    <Route exact path="/" component={HomePage}>
-      Home
-    </Route>
-    <Switch>
-      <Route path="/browse" component={BrowsePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/movieform" component={MovieForm} />
-      <Route path="/chat" component={ChatWindow} />
-      <Route path="/login" render={(props) => <OAuth {...props} provider={provider} /> } />   
-    </Switch>
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+  }
+  
+  componentWillMount() {
+    const query = queryString.parse(this.props.location.search);
+    if (query.token) {
+      window.localStorage.setItem('jwt', query.token);
+      this.props.history.push('/');
+    }
+  }
+   
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={HomePage}>
+          Home
+        </Route>
+        <Switch>
+          <Route path="/browse" component={BrowsePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/movieform" component={MovieForm} />
+          <Route path="/chat" component={ChatWindow} />
+          <Route path="/login" render={(props) => <OAuth {...props} user={this.state.user} />} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,135 +1,40 @@
-import React, { Component } from 'react';
-import queryString from 'query-string';
+import React from 'react';
 
-// const API_URL = 'https://yamovie-server.herokuapp.com/auth/';
-const API_URL = 'http://localhost:5000/auth';
-
-class OAuth extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
-      disabled: '',
-    };
-  }
-  componentWillMount() {
-    var query = queryString.parse(this.props.location.search);
-    if (query.token) {
-      window.localStorage.setItem("jwt", query.token);
-      this.props.history.push("/");
-   }
-}
-
-  // componentDidMount() {
-  //   const { socket, provider } = this.props;
-
-  //   socket.on(provider, user => {
-  //     this.popup.close();
-  //     this.setState({ user });
-  //   });
-  // }
-
-  // Routinely checks the popup to re-enable the login button 
-  // if the user closes the popup without authenticating.
-  // checkPopup = () => {
-  //   const check = setInterval(() => {
-  //     const { popup } = this;
-  //     if (!popup || popup.closed || popup.closed === undefined) {
-  //       clearInterval(check);
-  //       this.setState({ disabled: '' });
-  //     }
-  //   }, 1000);
-  // }
-
-  // Launches the popup by making a request to the server and then 
-  // passes along the socket id so it can be used to send back user 
-  // data to the appropriate socket on the connected client.
-  // openPopup = () => {
-  //   // const { provider, socket } = this.props;
-  //   const { disabled, user } = this.state;
-  //   const width = 600; const 
-  //     height = 600;
-  //   const left = (window.innerWidth / 2) - (width / 2);
-  //   const top = (window.innerHeight / 2) - (height / 2);
-  //   const url = `${API_URL}/${provider}?socketId=${socket.id}`;
-
-  //   return window.open(url, '',
-  //     `toolbar=no, location=no, directories=no, status=no, menubar=no, 
-  //     scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
-  //     height=${height}, top=${top}, left=${left}`);
-  // }
-
-  // Kicks off the processes of opening the popup on the server and listening 
-  // to the popup. It also disables the login button so the user can not 
-  // attempt to login to the provider twice.
-  // startAuth = (e) => {
-  //   const { disabled } = this.state;
-  //   if (!disabled) {
-  //     e.preventDefault();
-  //     this.popup = this.openPopup();
-  //     this.checkPopup();
-  //     this.setState({ disabled: 'disabled' });
-  //   }
-  // }
-
-  // closeCard = () => {
-  //   this.setState({ user: {} });
-  // }
-
-  render() {
-    const { name, photo } = this.state.user;
-    const { provider } = this.props;
-    const { disabled } = this.state;
-    
-    // return (
-    //   <div>
-    //     {name
-    //       ? (
-    //         <div className="card">
-    //           <img src={photo} alt={name} />
-    //           <FontAwesome
-    //             name="times-circle"
-    //             className="close"
-    //             onClick={this.closeCard}
-    //           />
-    //           <h4>{name}</h4>
-    //         </div>
-    //       )
-    //       : (
-    //         <div className="button-wrapper fadein-fast">
-    //           <button
-    //             type="button"
-    //             onClick={this.startAuth.bind(this)}
-    //             className={`${provider} ${disabled} button`}
-    //           >
-    //             <FontAwesome
-    //               name={provider}
-    //             />
-    //           </button>
-    //         </div>
-    //       )
-    //     }
-    //   </div>
-    // );
-    return(
+const OAuth = () => (
+  <div>
+    <a href="http://localhost:5000/auth/google" className="button">
       <div>
-      <a href="http://localhost:5000/auth/google" class="button">
-          <div>
-            <span class="svgIcon t-popup-svg">
+      <span class="svgIcon t-popup-svg">
               <svg
                 class="svgIcon-use"
                 width="25"
                 height="37"
                 viewBox="0 0 25 25"
-                />
-                </span>
-            <span class="button-label">Sign in with Google</span>
-          </div>
-        </a>
+              >
+                <g fill="none" fill-rule="evenodd">
+                  <path
+                    d="M20.66 12.693c0-.603-.054-1.182-.155-1.738H12.5v3.287h4.575a3.91 3.91 0 0 1-1.697 2.566v2.133h2.747c1.608-1.48 2.535-3.65 2.535-6.24z"
+                    fill="#4285F4"
+                  />
+                  <path
+                    d="M12.5 21c2.295 0 4.22-.76 5.625-2.06l-2.747-2.132c-.76.51-1.734.81-2.878.81-2.214 0-4.088-1.494-4.756-3.503h-2.84v2.202A8.498 8.498 0 0 0 12.5 21z"
+                    fill="#34A853"
+                  />
+                  <path
+                    d="M7.744 14.115c-.17-.51-.267-1.055-.267-1.615s.097-1.105.267-1.615V8.683h-2.84A8.488 8.488 0 0 0 4 12.5c0 1.372.328 2.67.904 3.817l2.84-2.202z"
+                    fill="#FBBC05"
+                  />
+                  <path
+                    d="M12.5 7.38c1.248 0 2.368.43 3.25 1.272l2.437-2.438C16.715 4.842 14.79 4 12.5 4a8.497 8.497 0 0 0-7.596 4.683l2.84 2.202c.668-2.01 2.542-3.504 4.756-3.504z"
+                    fill="#EA4335"
+                  />
+                </g>
+              </svg>
+            </span>
+        <span className="button-label">Sign in with Google</span>
       </div>
-    )
-  }
-}
-
+    </a>
+  </div>
+);
 
 export default OAuth;
