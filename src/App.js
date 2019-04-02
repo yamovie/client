9ee37129bdb620b42a6c9dc1cb9ecdaa3e8902e9
Ivy <yunjilim@gmail.com>
2019-04-02 07:41:55 -0700

    oauth working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import openSocket from 'socket.io-client';
+// import openSocket from 'socket.io-client';
 import HomePage from './pages/HomePage';
 import BrowsePage from './pages/BrowsePage';
 import AboutPage from './pages/AboutPage';
@@ -13,7 +13,7 @@ import './css/main.css';
 const provider = 'google';
 // const API_URL = 'https://yamovie-server.herokuapp.com/auth';
 // const socket = io('http://localhost');
-const socket = openSocket('http://localhost:80');
+// const socket = openSocket('http://localhost:80');
 
 const App = () => (
   <div className="App">
@@ -25,7 +25,7 @@ const App = () => (
       <Route path="/about" component={AboutPage} />
       <Route path="/movieform" component={MovieForm} />
       <Route path="/chat" component={ChatWindow} />
-      <Route path="/login" render={(props) => <OAuth {...props} provider={provider} socket={socket} /> } />   
+      <Route path="/login" render={(props) => <OAuth {...props} provider={provider} /> } />   
     </Switch>
   </div>
 );
--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import FontAwesome from 'react-fontawesome';
+import queryString from 'query-string';
 
 // const API_URL = 'https://yamovie-server.herokuapp.com/auth/';
 const API_URL = 'http://localhost:5000/auth';
@@ -12,98 +12,122 @@ class OAuth extends Component {
       disabled: '',
     };
   }
+  componentWillMount() {
+    var query = queryString.parse(this.props.location.search);
+    if (query.token) {
+      window.localStorage.setItem("jwt", query.token);
+      this.props.history.push("/");
+   }
+}
 
-  componentDidMount() {
-    const { socket, provider } = this.props;
+  // componentDidMount() {
+  //   const { socket, provider } = this.props;
 
-    socket.on(provider, user => {
-      this.popup.close();
-      this.setState({ user });
-    });
-  }
+  //   socket.on(provider, user => {
+  //     this.popup.close();
+  //     this.setState({ user });
+  //   });
+  // }
 
   // Routinely checks the popup to re-enable the login button 
   // if the user closes the popup without authenticating.
-  checkPopup = () => {
-    const check = setInterval(() => {
-      const { popup } = this;
-      if (!popup || popup.closed || popup.closed === undefined) {
-        clearInterval(check);
-        this.setState({ disabled: '' });
-      }
-    }, 1000);
-  }
+  // checkPopup = () => {
+  //   const check = setInterval(() => {
+  //     const { popup } = this;
+  //     if (!popup || popup.closed || popup.closed === undefined) {
+  //       clearInterval(check);
+  //       this.setState({ disabled: '' });
+  //     }
+  //   }, 1000);
+  // }
 
   // Launches the popup by making a request to the server and then 
   // passes along the socket id so it can be used to send back user 
   // data to the appropriate socket on the connected client.
-  openPopup = () => {
-    const { provider, socket } = this.props;
-    const { disabled, user } = this.state;
-    const width = 600; const 
-      height = 600;
-    const left = (window.innerWidth / 2) - (width / 2);
-    const top = (window.innerHeight / 2) - (height / 2);
-    const url = `${API_URL}/${provider}?socketId=${socket.id}`;
+  // openPopup = () => {
+  //   // const { provider, socket } = this.props;
+  //   const { disabled, user } = this.state;
+  //   const width = 600; const 
+  //     height = 600;
+  //   const left = (window.innerWidth / 2) - (width / 2);
+  //   const top = (window.innerHeight / 2) - (height / 2);
+  //   const url = `${API_URL}/${provider}?socketId=${socket.id}`;
 
-    return window.open(url, '',
-      `toolbar=no, location=no, directories=no, status=no, menubar=no, 
-      scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
-      height=${height}, top=${top}, left=${left}`);
-  }
+  //   return window.open(url, '',
+  //     `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+  //     scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+  //     height=${height}, top=${top}, left=${left}`);
+  // }
 
   // Kicks off the processes of opening the popup on the server and listening 
   // to the popup. It also disables the login button so the user can not 
   // attempt to login to the provider twice.
-  startAuth = (e) => {
-    const { disabled } = this.state;
-    if (!disabled) {
-      e.preventDefault();
-      this.popup = this.openPopup();
-      this.checkPopup();
-      this.setState({ disabled: 'disabled' });
-    }
-  }
+  // startAuth = (e) => {
+  //   const { disabled } = this.state;
+  //   if (!disabled) {
+  //     e.preventDefault();
+  //     this.popup = this.openPopup();
+  //     this.checkPopup();
+  //     this.setState({ disabled: 'disabled' });
+  //   }
+  // }
 
-  closeCard = () => {
-    this.setState({ user: {} });
-  }
+  // closeCard = () => {
+  //   this.setState({ user: {} });
+  // }
 
   render() {
     const { name, photo } = this.state.user;
     const { provider } = this.props;
     const { disabled } = this.state;
     
-    return (
+    // return (
+    //   <div>
+    //     {name
+    //       ? (
+    //         <div className="card">
+    //           <img src={photo} alt={name} />
+    //           <FontAwesome
+    //             name="times-circle"
+    //             className="close"
+    //             onClick={this.closeCard}
+    //           />
+    //           <h4>{name}</h4>
+    //         </div>
+    //       )
+    //       : (
+    //         <div className="button-wrapper fadein-fast">
+    //           <button
+    //             type="button"
+    //             onClick={this.startAuth.bind(this)}
+    //             className={`${provider} ${disabled} button`}
+    //           >
+    //             <FontAwesome
+    //               name={provider}
+    //             />
+    //           </button>
+    //         </div>
+    //       )
+    //     }
+    //   </div>
+    // );
+    return(
       <div>
-        {name
-          ? (
-            <div className="card">
-              <img src={photo} alt={name} />
-              <FontAwesome
-                name="times-circle"
-                className="close"
-                onClick={this.closeCard}
-              />
-              <h4>{name}</h4>
-            </div>
-          )
-          : (
-            <div className="button-wrapper fadein-fast">
-              <button
-                type="button"
-                onClick={this.startAuth.bind(this)}
-                className={`${provider} ${disabled} button`}
-              >
-                <FontAwesome
-                  name={provider}
+      <a href="http://localhost:5000/auth/google" class="button">
+          <div>
+            <span class="svgIcon t-popup-svg">
+              <svg
+                class="svgIcon-use"
+                width="25"
+                height="37"
+                viewBox="0 0 25 25"
                 />
-              </button>
-            </div>
-          )
-        }
+                </span>
+            <span class="button-label">Sign in with Google</span>
+          </div>
+        </a>
       </div>
-    );
+    )
   }
 }
 
