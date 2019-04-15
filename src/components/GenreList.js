085043fa3137ddb991a3b57d48f6fc631e34f451
Kevin Baktiar <afterafx@gmail.com>
2019-04-15 16:13:50 -0700

    added isActive highlight when filtering by genre

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -3,23 +3,53 @@ import PropTypes from 'prop-types';
 import '../css/GenreList.css';
 
 // Renders the genre list to the MovieList page. ==================
-const GenreList = ({ moviesByGenreId, style, genres }) => (
-  <div id="list-genres" style={style}>
-    <button className="single-genre" type="button" onClick={() => moviesByGenreId('all')}>
-      All
-    </button>
-    {genres.map(genre => (
-      <button
-        className="single-genre"
-        type="button"
-        key={genre.name}
-        onClick={() => moviesByGenreId(genre._id)}
-      >
-        {genre.name}
-      </button>
-    ))}
-  </div>
-);
+class GenreList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeButton: '',
+    }
+  }
+
+  handleActiveButton = genreName => {
+    const { active } = this.state;
+    if (genreName) {
+      this.setState({
+        activeButton: genreName,
+      });
+    } else {
+      this.setState({
+        activeButton: '',
+      });
+    }
+  }
+  
+  
+  render() {
+    const { moviesByGenreId, style, genres } = this.props;
+    const { activeButton } = this.state;
+    return (
+      <div id="list-genres" style={style}>
+        <button className="single-genre" type="button" onClick={() => moviesByGenreId('all')}>
+          All
+        </button>
+        {genres.map(genre => (
+          <button
+            className="single-genre"
+            type="button"
+            key={genre.name}
+            style={activeButton === genre.name ? { backgroundColor: '#88388c' } : { backgroundColor: 'rgba(226, 217, 217, 0.0)'} }
+            onClick={() => { moviesByGenreId(genre._id); this.handleActiveButton(genre.name); } }
+          >
+            {genre.name}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
+
 
 GenreList.propTypes = {
   genres: PropTypes.arrayOf(PropTypes.object).isRequired,
