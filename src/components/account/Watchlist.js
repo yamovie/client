9bf24095fa2997f695b98c00f4a03bfdf43bf691
Hans Martin <hansmhank@gmail.com>
2019-05-16 13:34:04 -0700

    Added css transition component

--- src/components/account/Watchlist.js
+++ src/components/account/Watchlist.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import '../../css/account/Watchlist.css';
 import { WatchlistItem } from '..';
+import { CSSTransition, Transition, TransitionGroup } from 'react-transition-group';
+
 // import { watch } from 'fs';
 
 const { REACT_APP_SVR_USERS } = process.env;
@@ -22,6 +24,7 @@ class Watchlist extends React.Component {
   }
 
   componentDidMount() {
+    console.log('did mount');
     const { user } = this.props;
     axios.get(`${REACT_APP_SVR_USERS}/watchlist/${user._id}`)
       .then(res => this.setState({movies: res.data}));
@@ -30,13 +33,17 @@ class Watchlist extends React.Component {
   removeFromWatchlist(e, movieId) {
     const { user } = this.props;
     const { movies } = this.state;
-    axios.delete(`${REACT_APP_SVR_USERS}/watchlist/${user._id}`, {data: { movieId } })
-      .then(() => {
-        this.setState({movies: movies.filter(movie => movie._id !== movieId)});
-      })
-      .catch(error => {
-        console.log(`The server responded with error: ${error.response.status}, ${error.response.statusText} `)
-      });
+    const newMovies = movies.filter(movie => movie._id !== movieId);
+    this.setState({movies: newMovies });
+    console.log('removed');
+
+    // axios.delete(`${REACT_APP_SVR_USERS}/watchlist/${user._id}`, {data: { movieId } })
+    //   .then(() => {
+    //     this.setState({movies: movies.filter(movie => movie._id !== movieId)});
+    //   })
+    //   .catch(error => {
+    //     console.log(`The server responded with error: ${error.response.status}, ${error.response.statusText} `)
+    //   });
   }
 
   render() {
@@ -45,15 +52,13 @@ class Watchlist extends React.Component {
     return (
       <div className="account-pane">
         <h1 className='account-title'>Your Watchlist</h1>
-        {
-          <div className="watchlist-wrapper">
-            {movies.length > 1 && (
-              movies.map((movie, i) => (
-                <WatchlistItem movie={movie} speed={50} multiplier={i} remove={this.removeFromWatchlist} />
-              ))
-            )}
-          </div>
-        }
+        <TransitionGroup className="watchlist-wrapper" transitionName="item" transitionEnterTimeout={700} transitionLeaveTimeout={700}>
+          {movies.length > 1 && (
+            movies.map((movie, i) => (
+              <WatchlistItem movie={movie} speed={50} multiplier={i} remove={this.removeFromWatchlist} />
+            ))
+          )}
+        </TransitionGroup>
       </div>
     );
   }
--- src/components/account/WatchlistItem.js
+++ src/components/account/WatchlistItem.js
@@ -1,52 +1,53 @@
 import React from 'react';
 import { faMinusCircle } from '@fortawesome/free-solid-svg-icons';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, Transition, TransitionGroup } from 'react-transition-group';
 import { FontAwesomeIcon } from '../../utils/fontAwesome';
 
 export default class WatchlistItem extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      render: false,
-      set: false,
-
+      showMovie: false,
+      runRemoveAnimation: false,
     }
   }
 
   componentDidMount() {
     const { speed, multiplier } = this.props;
     setTimeout(() => {
-      this.setState({render: true})
+      this.setState({showMovie: true})
     }, speed * multiplier)
+  }
+
+  runRemoveAnimation() {
+
+    this.setState({runRemoveAnimation: true})
   }
   
   render() {
     const { movie, remove } = this.props;
-    const { render } = this.state;
+    const { showMovie, runRemoveAnimation } = this.state;
     const style = {
       WebkitAnimation: 'fadeIn 2s',
       animation: 'fadeIn 2s',
     }
 
-    return  (
-      render && (
-        <CSSTransition
-        // in={showMessage}
-          // timeout={300}
-          classNames="collapse-left"
-          // unmountOnExit
-          // onEnter={() => setShowButton(false)}
-          onExited={(e) => remove(e, movie.id)}
-        >
-          <div className="watchlist-movie" style={style}>
-            <img src={movie.images.poster} className="img-fluid" alt="movie" />
-            <button type="button" className="watchlist-remove-btn" onClick={(e) => remove(e, movie._id)}>
-              <FontAwesomeIcon icon={faMinusCircle} /> Remove
-            </button>
-          </div>
-        </CSSTransition>
-      )
-    )
+    if (!showMovie) {
+      return <div />
+    }
     
+    return  (
+      // <CSSTransition
+      //   timeout={500}
+      //   classNames="item"
+      // >
+      <div className="watchlist-movie" style={style}>
+        <img src={movie.images.poster} className="img-fluid" alt="movie" />
+        <button type="button" className="watchlist-remove-btn" onClick={(e) => remove(e, movie._id)}>
+          <FontAwesomeIcon icon={faMinusCircle} /> Remove
+        </button>
+      </div>
+      // </CSSTransition>
+    );
   }
 }
